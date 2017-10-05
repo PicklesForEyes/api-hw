726a6fd4c465e18e7e6f9632e73ca02edf50d30e
Benjamin Lawson <lawbe101@yahoo.com>
2017-10-05 13:27:29 -0400

    added for loop to test appending buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
-console.log('I get here');
+$(document).ready(function() {
+  
+  var buttons = ['java', 'coffee', 'espresso'];
+
+  for(var i = 0; i < buttons.length; i++) {
+    var btn = $('<button>'+ buttons[i] + '</button>');
+      btn.attr('data-value', buttons[i]);
+      // btn.text(buttons[i]);
+      btn.addClass('btn btn-success');
+    $('#button-display').append(btn);
+  }
+
+})
