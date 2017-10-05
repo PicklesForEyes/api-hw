78d03a537db025430091e771c6230d023ed19f0a
Benjamin Lawson <lawbe101@yahoo.com>
2017-10-05 14:07:21 -0400

    iteration through data obj. var for urls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,16 +25,21 @@ $(document).ready(function() {
   $(document).on('click', '.btn-success', function() {
     var btnVal = $(this).attr('data-value');
     // console.log(btnVal);
-
     var queryUrl = 'http://api.giphy.com/v1/gifs/search?q=' + btnVal + '&limit=10&api_key=cxfIQfJBUKH9fFxsLp5sSwLBOICRb8Ud';
-
     // console.log(queryUrl);
 
     $.ajax({
       url: queryUrl,
       method: 'GET'
     }).done(function(obj) {
-      console.log(obj);
+      console.log(obj.data);
+      for(var i = 0; i < obj.data.length; i++){
+        var current = obj.data[i].images
+        var still = current.original_still.url;
+        var live = current.original.url;
+        console.log(current);
+        
+      }
     })
   })
 
